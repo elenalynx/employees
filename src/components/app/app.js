d66b613787377a1b71c2ba363945f51ff9576cc9
elenalynx <elnlynx@gmail.com>
2023-04-02 14:27:00 +0300

    create prj employees

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+import './app.css';
+
+function App() {
+    const data = [
+        {name: 'John C.1', salary: 800, increase: false},
+        {name: 'John C.2', salary: 2800, increase: true},
+        {name: 'John C.3', salary: 1800, increase: false},
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,21 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import './employees-list.css';
+
+const EmployeesList = ({data}) => {
+    const elements = data.map(item => {
+        return(
+            <EmployeesListItem {...item}/>
+            // <EmployeesListItem name={item.name} salary={item.salary}/>
+        )
+    });
+
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList;
